7dc7ede4fee8c4c74ac2b45d0e8d3d38c08d7537
SlayerArman <cartoontv123360@gmail.com>
2026-08-26 18:42:11 +0500

    added covers & music

---
+++ script.js
@@ -0,0 +1,20 @@
+const songList = [
+    {
+        name: "Play Date (Slowed)",
+        artist: "Melanie Martinez",
+        src: "assets/1.mp3",
+        cover: "assets/1.jpg"
+    },
+    {
+        name: "BEUNATEKK",
+        artist: "Ya Boi Ivan",
+        src: "assets/2.mp3",
+        cover: "assets/2.jpg"
+    },
+    {
+        name: "Around The World",
+        artist: "Sabrina Carpenter",
+        src: "assets/3.mp3",
+        cover: "assets/3.jpg"
+    }
+]
