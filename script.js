577c1582e66325cc101661b4fb454c1ed182f036
SlayerArman <cartoontv123360@gmail.com>
2026-08-27 09:30:55 +0500

    constants setup

--- script.js
+++ script.js
@@ -17,4 +17,14 @@ const songList = [
         src: "assets/3.mp3",
         cover: "assets/3.jpg"
     }
-]
+];
+
+const atristName = document.querySelector('.artisat-name');
+const musicName = document.querySelector('.song-name');
+const fillBar = document.querySelector('.fill-bar');
+const time = document.querySelector('time');
+const cover = document.getElementById('.cover');
+const playBtn = document.getElementById('.play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const prog = document.querySelector('.progress-bar');
